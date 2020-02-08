92702e802c61ea34b624423f0c77ea4386b3064f
Sinai <sina.shanibeik@gmail.com>
2020-02-08 17:51:25 +0330

    Updates

---
+++ precache-manifest.e6f7f0ade1ca867d6f51f962925ff879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd72c13c63f674f40bb8162c68680a2d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c21d9f80719d47f1af2",
+    "url": "/monsters-rolodex/static/css/main.11b0333c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c21d9f80719d47f1af2",
+    "url": "/monsters-rolodex/static/js/main.5654e9ef.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
